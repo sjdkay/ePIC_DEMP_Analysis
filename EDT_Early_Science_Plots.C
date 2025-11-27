f581e5dcd7d9eefdc0d2e22d3240ab62b22f4b43
sjdkay <sjdkay@gmail.com>
2025-11-27 07:51:06 -0500

    Updated colours on early science paper plots. Need to add Delta Theta* plot creation.

--- EDT_Early_Science_Plots.C
+++ EDT_Early_Science_Plots.C
@@ -23,24 +23,56 @@
 #include <TLatex.h>
 #include <TLegend.h>
 #include "ePICStyle.C"
+#include <vector>
 
-void EDT_Early_Science_Plots(string InFilename = ""){
+void ePIC_Plot(Double_t TextXPos, Double_t TextYPos, Double_t LogoXPos, Double_t LogoYPos, TString BeamE){
+  TLatex Text_com;
+  Text_com.SetTextSize(0.04);
+  Text_com.SetTextFont(42);
+  Text_com.SetTextAlign(13);  //align at top
+  Text_com.DrawLatexNDC(TextXPos,TextYPos-0.04,Form("e+p, %s(e)x%s(p)", ((TObjString *)((BeamE.Tokenize("on"))->At(0)))->String().Data(), ((TObjString *)((BeamE.Tokenize("on"))->At(1)))->String().Data()));
+  Text_com.DrawLatexNDC(TextXPos,TextYPos-0.08,"25.10.2 Campaign");
+  Text_com.DrawLatexNDC(TextXPos,TextYPos-0.12,"DEMPgen 1.2.4");
+  Text_com.DrawLatexNDC(TextXPos,TextYPos-0.16,"DEMP ep#rightarrow e'n#pi^{+}");
+  TLatex Text_ePIC;
+  Text_ePIC.SetTextSize(0.04);
+  Text_ePIC.SetTextFont(62);
+  Text_ePIC.DrawLatexNDC(TextXPos,TextYPos,"ePIC Performance");  // performance plot
+  //Text_ePIC.DrawLatexNDC(.15,.88,"ePIC Internal");  // for internal use only
+  //Text_ePIC.DrawLatexNDC(.15,.88,"ePIC Preliminary"); // preliminary released version
+  //Text_ePIC.DrawLatexNDC(.15,.88,"ePIC Work in Progress"); // work in progress to be shown outside
+  //Text_ePIC.DrawLatexNDC(.15,.88,"ePIC"); // final published version
+  // ===== Add ePIC logo to the figure if desired ======
+  TImage *logo = TImage::Open("EPIC-logo_black_small.png");
+  TPad *pad2 = new TPad("pad2", "Pad 2", LogoXPos, LogoYPos, LogoXPos + 0.13, LogoYPos + 0.13); // Create a new pad and then draw the image in it
+  pad2->Draw();
+  pad2->cd(); // Enter the new pad
+  logo->Draw();
+}
+
+void EDT_Early_Science_Plots(string InFilename = "", TString InBeamE =""){
 
   gROOT->SetBatch(kTRUE); // Force script to always run without flashing up graphics
-  //gROOT->ProcessLine("SetECCEStyle()");
+  gROOT->ProcessLine("SetePICStyle()");
   gStyle->SetOptStat(0);
   gStyle->SetPadLeftMargin(0.15);
   gStyle->SetPadRightMargin(0.15);
+  TString kp6[6] = {"#7021dd", "#9c9ca1", "#964a8b", "#e42536", "#f89c20", "#5790fc"};
   TString EDT_EarlyScience_rootFile;
-
+  
   if(InFilename == ""){
     cout << "Enter a calorimeter filename to analyse: ";
     cin >> InFilename;
   }
+
+  if (InBeamE == ""){
+    cout << "Enter a beam energy combination in the format - XonY - where X is the electron beam energy in GeV and Y is the proton beam energy:" << endl;
+    cin >> InBeamE;
+  }
   
   TString TInFilename = InFilename;
   EDT_EarlyScience_rootFile = TInFilename;
-
+  
   if(gSystem->AccessPathName(EDT_EarlyScience_rootFile) == kTRUE){
     cerr << "!!!!! ERROR !!!!!" << endl << EDT_EarlyScience_rootFile << " not found" << endl << "!!!!! ERROR !!!!!" << endl;
     exit(0);
@@ -79,26 +111,28 @@ void EDT_Early_Science_Plots(string InFilename = ""){
   else{
     Output_Name = TInFilename.ReplaceAll(".root","");
   }
-  
+
   TCanvas *c_tDist = new TCanvas("c_tDist", "t Distribution", 100, 0, 2560, 1920);
-  tMC_Dist->SetMarkerColor(1); tMC_Dist->SetLineColor(1);
+  tMC_Dist->SetMarkerColor(TColor::GetColor(kp6[0].Data())); tMC_Dist->SetLineColor(TColor::GetColor(kp6[0].Data()));
   tMC_Dist->GetXaxis()->SetRangeUser(0, 0.5);
+  tMC_Dist->GetXaxis()->SetTitleOffset(1);
   tMC_Dist->Draw("Histerr");
-  tResult_Dist->SetMarkerColor(2); tResult_Dist->SetLineColor(2);
-  tResult_Scaled_Dist->SetMarkerColor(4); tResult_Scaled_Dist->SetLineColor(4);
-  tMCResult_Dist->SetMarkerColor(8); tMCResult_Dist->SetLineColor(8);
+  tResult_Dist->SetMarkerColor(TColor::GetColor(kp6[4].Data())); tResult_Dist->SetLineColor(TColor::GetColor(kp6[4].Data()));
+  tResult_Scaled_Dist->SetMarkerColor(TColor::GetColor(kp6[3].Data())); tResult_Scaled_Dist->SetLineColor(TColor::GetColor(kp6[3].Data()));
+  tMCResult_Dist->SetMarkerColor(TColor::GetColor(kp6[2].Data())); tMCResult_Dist->SetLineColor(TColor::GetColor(kp6[2].Data()));
   tResult_Dist->Draw("Samehisterr");
   tResult_Scaled_Dist->Draw("Samehisterr");
   tMCResult_Dist->Draw("samehisterr");
-  TLegend *t_Legend = new TLegend(0.6, 0.65, 0.8, 0.85);
+  TLegend *t_Legend = new TLegend(0.6, 0.5, 0.8, 0.7);
   t_Legend->SetTextSize(0.04);
   t_Legend->AddEntry(tMC_Dist, "MC Truth", "l");
   t_Legend->AddEntry(tResult_Dist, "Reco Raw", "l");
   t_Legend->AddEntry(tResult_Scaled_Dist, "Reco Corr", "l");
   t_Legend->AddEntry(tMCResult_Dist, "MC Accept", "l");
   t_Legend->Draw("SAME");
-  
+  ePIC_Plot(0.35, 0.88, 0.87, 0.8, InBeamE);  
   c_tDist->Print(Output_Name+".pdf");
+  c_tDist->Print(Output_Name+".png");
 
   TFile *OutFile = new TFile(Output_Name+".root", "RECREATE");
   tMC_Dist->Write();
@@ -110,3 +144,4 @@ void EDT_Early_Science_Plots(string InFilename = ""){
   InFile->Close();
 
 }
+
